7f892bcbd6bc41a227b3639c15d7650922bf3079
sparkels36 <piyush362002@gmail.com>
2024-08-30 23:03:21 +0530

    middleware structured

---
+++ Backend/wild_life/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class WildLifeConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'wild_life'
---
+++ Backend/wild_life/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from .serializers import *
+from .models import *
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
