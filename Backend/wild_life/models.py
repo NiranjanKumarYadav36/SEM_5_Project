deaec41accc1e89df8fa3b081d685ed34a488ad5
sparkels36 <piyush362002@gmail.com>
2024-09-07 23:56:29 +0530

    middleware structured

--- Backend/wild_life/models.py
+++ Backend/wild_life/models.py
@@ -1,9 +1,4 @@
 from django.db import models
 
 
-class Item(models.Model):
-    names = models.CharField(max_length=100)
-    description = models.TextField()
 
-    def __str__(self):
-        return self.names
--- Backend/wild_life/views.py
+++ Backend/wild_life/views.py
@@ -1,11 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from .models import Item
-from .serializers import ItemSerializer
+from .models import * # ()
+from .serializers import *
 
 
-class ItemList(APIView):
-    def get(self, request):
-        items = Item.objects.all()
-        serializer = ItemSerializer(items, many=True)
-        return Response(serializer.data)
