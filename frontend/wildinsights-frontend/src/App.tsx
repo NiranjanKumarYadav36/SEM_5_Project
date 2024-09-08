f1d246b63792f296bbde1e2b7ce2ab8a78df149f
itsmegravity <122367685+itsmegravity@users.noreply.github.com>
2024-09-08 15:52:12 +0530

    Fix:Integration

--- frontend/wildinsights-frontend/src/App.tsx
+++ frontend/wildinsights-frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import React from 'react'
+import ItemList from './components/ItemList'
 import './App.css'
 
 function App() {
@@ -25,6 +26,9 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
+      </div>
+      <div>
+        <ItemList></ItemList>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
