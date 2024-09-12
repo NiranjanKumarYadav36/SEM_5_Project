f0cd9e3f7a1d4d207788fb82e307b6f719eab281
sparkels36 <piyush362002@gmail.com>
2024-09-12 14:55:31 +0530

    added logic for jwt token

--- Backend/wild_life/urls.py
+++ Backend/wild_life/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import RegisterView, LoginView, UserView
+from .views import RegisterView, LoginView, UserView, Logout
 
 urlpatterns = [
     path('register', RegisterView.as_view(), name='register'),
     path('login', LoginView.as_view(), name='login'),
     path('user_view', UserView.as_view(), name='user_view'),
+    path('logout', Logout.as_view(), name='logout'),
+
 ]
 
