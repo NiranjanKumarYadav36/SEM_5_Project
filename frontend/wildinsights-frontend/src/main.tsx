8edcd5b1901dead7a66e19c7bc9ddcd3b6c2d50e
itsmegravity <122367685+itsmegravity@users.noreply.github.com>
2024-09-18 22:34:45 +0530

    Fix:Register & added packages

--- frontend/wildinsights-frontend/src/main.tsx
+++ frontend/wildinsights-frontend/src/main.tsx
@@ -1,18 +1,26 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import React from 'react';
-import { RouterProvider } from 'react-router-dom';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import React from "react";
+import { RouterProvider } from "react-router-dom";
 import router from "./routes/router.tsx";
-import { ThemeProvider } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
+import {
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
 import theme from "./components/Themes/Theme.tsx";
 
+// Create a client
+const queryClient = new QueryClient();
 
-createRoot(document.getElementById('root')!).render(
-      <StrictMode>
-        <ThemeProvider theme={theme}>
-          <CssBaseline />
-          <RouterProvider router={router} />
-        </ThemeProvider>
-      </StrictMode>,
-)
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <ThemeProvider theme={theme}>
+      <QueryClientProvider client={queryClient}>
+        <CssBaseline />
+        <RouterProvider router={router} />
+      </QueryClientProvider>
+    </ThemeProvider>
+  </StrictMode>
+);
