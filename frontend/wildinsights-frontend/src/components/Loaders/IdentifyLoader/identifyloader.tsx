6401fe4e70fd55bf64fa5e46cecd7308bc68070e
itsmegravity <122367685+itsmegravity@users.noreply.github.com>
2024-10-02 17:32:19 +0530

    Indentify logic

--- frontend/wildinsights-frontend/src/components/Loaders/IdentifyLoader/identifyloader.tsx
+++ frontend/wildinsights-frontend/src/components/Loaders/IdentifyLoader/identifyloader.tsx
@@ -12,7 +12,10 @@ interface IdentifyData {
   no_identification_disagreement: number;
 }
 
-export const useIdentifyLoader = (initialPage: number = 1,isReviewed: boolean = false) => {
+export const useIdentifyLoader = (
+  initialPage: number = 1, 
+  isReviewed: boolean = false
+) => {
   const [data, setData] = useState<IdentifyData[]>([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
@@ -25,11 +28,11 @@ export const useIdentifyLoader = (initialPage: number = 1,isReviewed: boolean = 
       const endpoint = isReviewed ? "reviewed" : "species_identifications/";
       const response = await axiosclient.get(`${endpoint}?page=${pageNumber}&page_size=12`);
       const newData = response.data || [];
-      console.log(newData)
-  
-      // Append new data to existing data
-      setData((prevData) => [...prevData, ...newData]); 
-  
+      console.log(newData);
+      
+      // Replace old data with the new data (no appending)
+      setData(newData); 
+
       // Check if there's a next page based on the API response
       setHasMore(response.data.next !== null);
     } catch (err) {
@@ -50,6 +53,6 @@ export const useIdentifyLoader = (initialPage: number = 1,isReviewed: boolean = 
       setPage((prevPage) => prevPage + 1);
     }
   };
-  // Expose current page for potential use in the component
+
   return { data, loading, error, loadMore, hasMore, page, setPage };
 };
